12bc84457b1222293822bbc62e60fc24ecd16f3e
azoong <shshinkitec2@gmail.com>
2022-08-19 16:55:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Router = require("./routes/index");
+
+
+const app = express();
+app.use(express.json());
+app.use("/", Router);
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message :"메인화면" })
+})
+
+module.exports = app;
+
+app.listen(8000, () => {
+  console.log(8000, "포트로 서버가 열렸어요!");
+});
