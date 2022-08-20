9ecdd67f51eacdc264d692d1a72f98f3af6d8633
azoong <shshinkitec2@gmail.com>
2022-08-20 22:14:19 +0900

    post update

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { Comment, User, Post } = require("../models");
 
+
+router.post("/:postId", async (req, res) => {
+    const { comment } = req.body;
+    const { postId } = req.params;
+    await Comment.create({ comment, postId });
+    res.status(201).json({
+        ok: true,
+        message: "댓글을 생성하였습니다.",
+    })
+})
 
 
 module.exports = router;
